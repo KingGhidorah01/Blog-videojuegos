a01b6a75b9b8648696b7650c0d3852fb1cc2f21f
KingGhidorah01 <martin.andrade@alumnos.udg.mx>
2021-06-23 18:04:06 -0500

    Primer commit

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Josué Andrade &copy; 2020</p>
+		</footer>
+		
+	</body>
+</html>
